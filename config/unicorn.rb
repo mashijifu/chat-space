68c4584da63753a851dab6ec2ebbdc1059c76b2e
藤島太智 <>
2018-06-05 15:33:55 +0900

    fix unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,23 +2,13 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-puts app_path
-p app_path
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-puts '------------------------------------------------'
-
 working_directory "#{app_path}/current"
-listen 3000
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
+listen 3000
 timeout 60
 
 preload_app true
