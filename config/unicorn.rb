c3490dcee43057417d437c842cf81f2a5849061e
藤島太智 <>
2018-06-05 13:36:13 +0900

    wip2

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -14,7 +14,7 @@ puts '------------------------------------------------'
 puts '------------------------------------------------'
 
 working_directory "#{app_path}/current"
-listen "#{app_path}/unicorn.sock"
+listen 3000
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
